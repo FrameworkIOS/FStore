45b75e438294e6ad478662aef78e32f2aef25ce0
krishpranav <krisna.pranav@gmail.com>
2023-07-09 16:12:23 +0530

    FStore: Entity[Nullable]

---
+++ FStore/Entity/Nullable.swift
@@ -0,0 +1,25 @@
+//
+//  Nullable.swift
+//  FStore
+//
+//  Created by krisna pranav on 09/07/23.
+//
+
+import Foundation
+
+public protocol Nullable {
+    associatedtype UnderlyingType
+    var wrappedValue: UnderlyingType? {
+        get
+    }
+    
+    init(_some: UnderlyingType)
+}
+
+extension Optional: Nullable {
+    public typealias UnderlyingType = Wrapped
+    
+    public var wrappedValue: UnderlyingType {
+        return map { $0 }
+    }
+}
