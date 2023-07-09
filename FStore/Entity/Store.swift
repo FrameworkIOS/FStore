539044ebb3c3f4a2bc45307385ad8659d9ae428c
krishpranav <krisna.pranav@gmail.com>
2023-07-09 16:10:05 +0530

    add files via upload

---
+++ FStore/Entity/Store.swift
@@ -0,0 +1,13 @@
+//
+//  Store.swift
+//  FStore
+//
+//  Created by krisna pranav on 09/07/23.
+//
+
+import Foundation
+
+/// store
+public protocol Store {
+    func clear()
+}
---
+++ FStore/FStore.h
@@ -0,0 +1,18 @@
+//
+//  FStore.h
+//  FStore
+//
+//  Created by krisna pranav on 09/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FStore.
+FOUNDATION_EXPORT double FStoreVersionNumber;
+
+//! Project version string for FStore.
+FOUNDATION_EXPORT const unsigned char FStoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FStore/PublicHeader.h>
+
+
