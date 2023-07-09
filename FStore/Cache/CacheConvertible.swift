cd95131bfb30b076dd4c251d2a2b325c873cb4de
krishpranav <krisna.pranav@gmail.com>
2023-07-09 16:33:22 +0530

    FStore: Cache[CacheConvertable]

--- FStore/Cache/CacheConvertible.swift
+++ FStore/Cache/CacheConvertible.swift
@@ -9,22 +9,27 @@ import Foundation
 
 public protocol CacheConvertible {
     associatedtype CacheType: CacheSupportedType
+    
     static func decode(cacheValue value: CacheType) -> Self?
-    var encodedForCache: CacheType? { get }
+    var encodeForCache: CacheType? { get }
 }
 
-struct CacheConvertibleBox<T: CacheConvertible>: CacheTransaction {
+
+struct CacheConvertibleBox <T: CacheConvertible>: CacheTransaction {
+    
     let value: T
     
     var supportedType: AnyObject? {
-        let anyObject: AnyObject? = value.encodedForCache
+        let anyObject: AnyObject? = value.encodeForCache
         return anyObject
     }
     
     init?(storedValue: AnyObject?) {
+        
         guard let cacheValue = storedValue as? T.CacheType,
-              let value = T.decode(cacheValue: cacheValue)
-        else {
+            let value = T.decode(cacheValue: cacheValue)
+            else
+        {
             return nil
         }
         
@@ -37,18 +42,25 @@ struct CacheConvertibleBox<T: CacheConvertible>: CacheTransaction {
 }
 
 
-struct CacheNullableConvertibleBox<T: Nullable>: CacheTransaction where T.UnderlyingType: CacheConvertible {
+struct CacheNullableConvertibleBox <T: Nullable>: CacheTransaction where T.UnderlyingType: CacheConvertible {
     let value: T
     
     var supportedType: AnyObject? {
-        return value.wrappedValue?.encodedForCache
+        return value.wrappedValue?.encodeForCaches
     }
     
     init?(storedValue: AnyObject?) {
-        guard let cacheValue = storedValue as T.UnderlyingType.CacheType,
-              let value = T.UnderlyingType.decode()
-        else {
-            return nil
+        guard let cacheValue = storedValue as? T.UnderlyingType.CacheType,
+            let value = T.UnderlyingType.decode(cacheValue: cacheValue)
+            else
+        {
+           return nil
         }
+        
+        self.value = T(value)
+    }
+    
+    init(_ value: T) {
+        self.value = value
     }
 }
